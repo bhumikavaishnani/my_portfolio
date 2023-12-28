44177df7817b1cebb1a466a83dccd1ee3a4472e1
Bhumika Vaishnani <bhumikavaishnani1990@gmail.com>
2023-12-28 16:33:31 -0500

    update code and resolve one warning

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,9 +13,7 @@ export default function Projects() {
             Websites I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+          Project Portfolio: Showcasing Innovation, Implementation, and Impact
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
