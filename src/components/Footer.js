bb86536215dfe17bcd5e70ff20cb5bbc81b99586
Bhumika Vaishnani <bhumikavaishnani1990@gmail.com>
2025-03-21 11:14:12 -0500

    change in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
             <section id="footer">
                 <div class="">
-                    <p><b>LinkedIn:</b> <a rel="noreferrer" href="https://www.linkedin.com/in/bhumikavaishnani/" target='_blank'>https://www.linkedin.com/in/bhumikakoradia/</a></p>
+                    <p><b>LinkedIn:</b> <a rel="noreferrer" href="https://www.linkedin.com/in/bhumikavaishnani/" target='_blank'>https://www.linkedin.com/in/bhumikavaishnani/</a></p>
                     <p><b>Email:</b> bhumikavaishnani1990@gmail.com</p>
                 </div>
             </section>
