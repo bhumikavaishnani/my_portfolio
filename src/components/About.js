e0e3331f45769b2762efa553f3782a997050a0f7
Bhumika Vaishnani <bhumikavaishnani1990@gmail.com>
2025-06-24 17:26:42 -0500

    Few changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,37 +2,37 @@ import React from 'react';
 
 export default function About() {
     return (
-            <section id="about">
-                <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, I'm Bhumika.
                         <br className="hidden lg:inline-block" />I love to build amazing websites.
                     </h1>
                     <p className="mb-8 leading-relaxed">
-                    As a senior developer with 9 years of professional experience, I have a robust background in React.js, Node.js, Vue.js, PHP, Laravel, Express.js, TypeScript, and Shopify. My diverse expertise extends across multiple domains, including Hospitality, Real Estate, Media, Job Portal, and Ecommerce. I possess hands-on experience in various domains such as cloud computing, blockchain technology and web development. Proficient in project deployment on AWS, I seamlessly navigate both Windows and Ubuntu (Linux) operating systems. My interactions with customers have significantly enhanced my communication skills, enabling me to confidently contribute to daily stand-up meetings and provide project updates. Feel free to connect with me on LinkedIn or via email if you are seeking a developer eager to show their skill set with considerable experience. I am consistently enthusiastic about taking new opportunities and applying my expertise to fresh endeavors.
+                        As a senior developer with 9 years of professional experience, I have a robust background in React.js, Node.js, Express.js, TypeScript, JavaScript, .NET Core, C#.Net, Vue.js, PHP, Laravel, and Shopify. My diverse expertise extends across multiple domains, including Hospitality, Real Estate, Media, Job Portal, and Ecommerce. I possess hands-on experience in various domains such as cloud computing, blockchain technology and web development. Proficient in project deployment on AWS, I seamlessly navigate both Windows and Ubuntu (Linux) operating systems. My interactions with customers have significantly enhanced my communication skills, enabling me to confidently contribute to daily stand-up meetings and provide project updates. Feel free to connect with me on LinkedIn or via email if you are seeking a developer eager to show their skill set with considerable experience. I am consistently enthusiastic about taking new opportunities and applying my expertise to fresh endeavors.
                     </p>
                     <div className="flex justify-center">
                         <a
-                        href="#contact"
-                        className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                        Work With Me
+                            href="#contact"
+                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            Work With Me
                         </a>
                         <a
-                        href="#projects"
-                        className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                        See My Past Work
+                            href="#projects"
+                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            See My Past Work
                         </a>
                     </div>
-                    </div>
-                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
                         src="./assets/bhumika.jpg"
                     />
-                    </div>
                 </div>
-            </section>
-            );
+            </div>
+        </section>
+    );
 }
--- src/data.js
+++ src/data.js
@@ -1,60 +1,56 @@
 export const projects = [
-    {
-      title: "Zweicoin",
-      subtitle: "Laravel & MySQL",
-      description:
-        "This website mainly shows all my client's digital currency related information. It will gives knowledge about NAMAZU and Autocalc app information to users. ",
-      image: "/assets/zweichain.png",
-      link: "https://zweichain.zweicoin.com/",
-    },
-    {
-      title: "Zweichain",
-      subtitle: "Laravel, React & MySQL",
-      description:
-        "This website is mainly shows publick blockchain network information of our client. It will shows all useful information about wallet system.  ",
-      image: "./assets/zweicoin.png",
-      link: "https://www.zweichain.net/",
-    },
-    {
-      title: "Hudosanya",
-      subtitle: "Laravel & MySQL",
-      description:
-        "This website mainly focus on assessment of property in Japan. It will provide approximate estimated value of property based on three main criterias.",
-      image: "./assets/hudosanya.png",
-      link: "http://www.hudosanya.com/",
-    },
-    {
-      title: "Cartly gifting",
-      subtitle: "MERN Stack and Shopify",
-      description:
-        "This website is e-commerce website mainly for gifts. People can send gifts based on occasions to thier dear ones. ",
-      image: "./assets/cartly-gifting.png",
-      link: "https://4b6877.myshopify.com/",
-    },
-  ];
+  {
+    title: "Zweicoin",
+    description:
+      "This website mainly shows all my client's digital currency related information. It will gives knowledge about NAMAZU and Autocalc app information to users. ",
+    image: "/assets/zweichain.png",
+    link: "https://zweichain.zweicoin.com/",
+  },
+  {
+    title: "Zweichain",
+    description:
+      "This website is mainly shows publick blockchain network information of our client. It will shows all useful information about wallet system.  ",
+    image: "./assets/zweicoin.png",
+    link: "https://www.zweichain.net/",
+  },
+  {
+    title: "Hudosanya",
+    description:
+      "This website mainly focus on assessment of property in Japan. It will provide approximate estimated value of property based on three main criterias.",
+    image: "./assets/hudosanya.png",
+    link: "http://www.hudosanya.com/",
+  },
+  {
+    title: "Cartly gifting",
+    description:
+      "This website is e-commerce website mainly for gifts. People can send gifts based on occasions to thier dear ones. ",
+    image: "./assets/cartly-gifting.png",
+    link: "https://4b6877.myshopify.com/",
+  },
+];
 
-  export const skills = [
-   "React.JS, Vue.JS, Node.js, Express.js, TypeScript",
-   "PHP, Laravel, Shopify, Wordpress, Symfony",
-   "MySQL, AWS RDBMS, SQL Server, POSTGreSQL, MongoDB",
-   "AWS, Azure, Netlify",
-   "OOP features, MVC, microservices architecture, CI/CD pipeline",
-   "JavaScript, HTML, jQuery, AJAX and CSS",
-   "RESTful API, microservices, POSTMAN, SDLC",
-   "Git, GitHub, GitLab, bitbucket",
-   "windows, ubuntu, linux",
-   "JIRA, Asana, trello"
-  ];
+export const skills = [
+  "React.JS, Node.js, Express, TypeScript, Javascript, Vue.js",
+  ".NET Core, C#.NET, PHP, Laravel, Shopify",
+  "MySQL, SQL Server, POSTGreSQL, AWS RDBMS, MongoDB",
+  "AWS, Azure, Netlify",
+  "OOP features, MVC, microservices architecture, CI/CD pipeline",
+  "JavaScript, HTML, jQuery, AJAX and CSS",
+  "RESTful API, microservices, POSTMAN, SDLC",
+  "Git, GitHub, GitLab, bitbucket",
+  "windows, ubuntu, linux",
+  "JIRA, Asana, trello"
+];
 
-  export const testimonials = [
-    {
-      name : "Hayato Kameta",
-      company : "zweispace",
-      quote : "Bhumika is one of the best, most reliable developer you'll come across. Her ability to write high-quality, accurate code for any website shows her dedication in job. Trust me, if I get a chance I'll hire her and work with her happily."
-    },
-    {
-      name : "Andrew Thomson",
-      company : "therapy sessions",
-      quote : "Bhumika is always ready to accept challenges in task. Her team is very understandable and supportive. She has always ready to do task or meetings according to my expectations. "
-    }
-  ];
+export const testimonials = [
+  {
+    name: "Hayato Kameta",
+    company: "zweispace",
+    quote: "Bhumika is one of the best, most reliable developer you'll come across. Her ability to write high-quality, accurate code for any website shows her dedication in job. Trust me, if I get a chance I'll hire her and work with her happily."
+  },
+  {
+    name: "Andrew Thomson",
+    company: "therapy sessions",
+    quote: "Bhumika is always ready to accept challenges in task. Her team is very understandable and supportive. She has always ready to do task or meetings according to my expectations. "
+  }
+];
